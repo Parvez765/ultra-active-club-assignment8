61e557c3e91699173485e398ad784c574b2ae4a7
Pervez <pervezhossain1998@gmail.com>
2022-09-28 22:55:11 +0600

    Timer Functionality Added

--- src/Components/Sports/Sports.js
+++ src/Components/Sports/Sports.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Sports.css"
 import myImage from "../../images/myImage-01.png"
 import Swal from 'sweetalert2'
 const Sports = () => {
 
-
-const activityCompleted = () => {
+    const [timer, setTimer] = useState(0)
+    
+    const activityCompleted = () => {
         Swal.fire(
     'Good job!',
     'You Have Achieve Your Goal',
@@ -24,16 +25,27 @@ const activityCompleted = () => {
                     <div className='information'>
                     <h2>Md. Parvez Hossain Sakib</h2>
                     <h5>Chattagram, Bangladesh</h5>
-                </div>
+                    </div>
+                    <div className='selfInfo'>
+                        <h3>75kg Weight</h3>
+                        <h3>5.2 Height</h3>
+                        <h3>24 Years</h3>
+                    </div>
                </div>
                 <div className='breakInfo'>
                     <h2>Add A Break</h2>
-                    <div className='timer'>
+                    <button className='btnAdd' onClick={() => setTimer(10)}>
                         <h3>10s</h3>
+                  </button>
+                    <button className='btnAdd' onClick={() => setTimer(20)}>
                         <h3>20s</h3>
+                  </button>
+                    <button className='btnAdd' onClick={() => setTimer(30)}>
                         <h3>30s</h3>
+                  </button>
+                    <button className='btnAdd' onClick={() => setTimer(40)}>
                         <h3>40s</h3>
-                    </div>
+                  </button>
                 </div>
                 <div className='excerciseDetails'>
                     <h2>Excercise Detail</h2>
@@ -45,8 +57,8 @@ const activityCompleted = () => {
                 <div className='excerciseDetails'>
                     <h2>Break Detail</h2>
                     <div className='excerciseTime'>
-                        <h3>Breaky Time</h3>
-                        <h3>0 Seconds</h3>
+                        <h3>Break Time</h3>
+                        <h3>{timer}</h3>
                     </div>
                 </div>
             <button className='completedBtn' onClick={activityCompleted}>Activity Completed</button>
