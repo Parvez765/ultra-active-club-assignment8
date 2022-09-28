4cf0e75804a4aaa642946b48e38cd9e838ec1fec
Pervez <pervezhossain1998@gmail.com>
2022-09-28 21:03:28 +0600

    Header Component Added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPersonWalking} from '@fortawesome/free-solid-svg-icons'
+import "./Header.css"
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='header'>
+            <FontAwesomeIcon icon={faPersonWalking} className="headerIcon"></FontAwesomeIcon>
+            <h1>Fitness Hero</h1>
         </div>
     );
 };
