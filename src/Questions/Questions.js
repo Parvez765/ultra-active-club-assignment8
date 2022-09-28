b5080b901e1fad0630ba6516a4c0b1580a837019
Pervez <pervezhossain1998@gmail.com>
2022-09-29 00:20:47 +0600

    question component finished

--- src/Questions/Questions.js
+++ src/Questions/Questions.js
@@ -2,4 +2,8 @@ Q-1: How Does React Work ?
 Answer - ReactJS divides the UI into isolated reusable pieces of code known as components.React components work similarly to JavaScript functions as they accept properties or props.
 
 Q-2: Difference Between Props and State
-Answer: Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components.
+Answer: Props are used to pass data from one component to another.The state is a local data storage that is local to the component only and cannot be passed to other components.
+
+
+Q - 3: What Else Does UseEffect do besides fecting API ? 
+Answer: The useEffect Hook allows to perform side effects in components. Besides fetching data it also does work on directly updating the DOM, and timers. useEffect accepts two arguments. The second argument is optional.    
