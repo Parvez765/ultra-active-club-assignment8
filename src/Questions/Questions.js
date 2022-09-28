a70d8ab00b67c2d0357883d1be8893d01d87c3b3
Pervez <pervezhossain1998@gmail.com>
2022-09-29 00:16:54 +0600

    question 2 added

--- src/Questions/Questions.js
+++ src/Questions/Questions.js
@@ -1,4 +1,5 @@
-Q - 1: How Does React Work ?
-A - ReactJS divides the UI into isolated reusable pieces of code known as components.React components work similarly to JavaScript functions as they accept properties or props.
+Q-1: How Does React Work ?
+Answer - ReactJS divides the UI into isolated reusable pieces of code known as components.React components work similarly to JavaScript functions as they accept properties or props.
 
-
+Q-2: Difference Between Props and State
+Answer: Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components.
